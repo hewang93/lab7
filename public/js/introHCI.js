2cd3b98ac4dff9f4a823288d3dfccdfda7c5b05e
Henry Wang <hewang@ucsd.edu>
2016-02-18 06:56:05 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$("button").click(projectClick);
+
+}
+
+function projectClick(e){
+	// prevent the page from reloading
+	e.preventDefault();
+
+	//In an event handler, $(this) refers to
+	// the object the triggered the event
+	ga("send", "event", 'like', 'click');
+
 }
